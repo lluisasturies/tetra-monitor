64415dffa56429cb46a3fd0c2ad793060389e909
Lluis de la Rubia <lluisasturies@gmail.com>
2026-03-05 17:44:26 +0100

    fix: Problemas en los scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,9 @@
 # Arranque manual del proyecto
 
 source venv/bin/activate
+cd src
 
 echo "Iniciando demonio PEI y grabación selectiva..."
-python3 src/pei_daemon.py &
+python3 pei_daemon.py &
 PID=$!
 echo "PID: $PID"
